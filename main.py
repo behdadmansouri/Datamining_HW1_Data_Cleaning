38fe385e765f2ca859a3cf5b13dfc95535e74e4c
Behdad Mnasouri <behdadmansouri@aut.ac.ir>
2022-04-08 21:46:19 +0430

    normalization done (though unsure

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import pandas as pd
-
+from sklearn import preprocessing
 
 def drop_nan(data):
-    print(data)
     print(data.isna().sum())
-    # count_nan = data['sepal_length'].isna().sum()
-    # print('Count of NaN: ' + str(count_nan))
-    # print(data.isnull().groupby(data['sepal_length', 'sepal_width', 'petal_length', 'petal_width']).sum().sum(axis=1))
-
     return data.dropna()
 
+
+def encode_data(data):
+    data['target'] = preprocessing.LabelEncoder().fit_transform(data['target'])
+    return data
+
+
+def normalize_data(data):
+    averages = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].mean()
+    variances = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].var()
+
+    target_temp = data.drop(['target'], axis=1, inplace=True)
+    standardized_data = preprocessing.StandardScaler().fit_transform(data)
+    standardized_data = pd.DataFrame(standardized_data, columns=[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+    standardized_data['target'] = target_temp
+
+    averages2 = standardized_data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].mean()
+    variances2 = standardized_data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].var()
+    for i in range(4):
+        print("column %2d" % (i + 1))
+        print("variance : %5.2f --> %5.2f" % (averages[i], averages2[i]))
+        print("mean     : %5.2f --> %5.2f" % (variances[i], variances2[i]))
+    return standardized_data
 
 def run_program(name):
     data = load_data(name)
     data = drop_nan(data)
-
+    data = encode_data(data)
+    print(data)
+    data = normalize_data(data)
+    print(data)
 
 def load_data(name):
     return pd.read_csv(name,
